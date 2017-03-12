96c87aad0b6dfe8c444effd8d1667f8a71a8ddcf
Zahi Akiva <zakiva@redhat.com>
2017-03-12 11:56:29 +0200

    Firebase Integration

--- app/src/main/java/com/example/zakiva/santacontrol/MainActivity.java
+++ app/src/main/java/com/example/zakiva/santacontrol/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.zakiva.santacontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.*;
+import com.google.firebase.database.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkFirebase();
+    }
+
+    public void checkFirebase () {
+        DatabaseReference myDatabase = FirebaseDatabase.getInstance().getReference();
+        myDatabase.child("santaControl").setValue("isWorking!");
     }
 }
